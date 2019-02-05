80793dd4c8b91eafe1b8fcaa4802e9cc012490ec
Akhat Gainutdinov <v-akgain@microsoft.com>
2019-02-05 18:33:28 +0300

    Pokemon project's skeleton

---
+++ homeworks/PokemonGoAlpha/app/src/main/java/com/example/pokemongoalpha/SquareImageView.java
@@ -0,0 +1,21 @@
+package com.example.pokemongoalpha;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+
+public class SquareImageView extends AppCompatImageView {
+    public SquareImageView(Context context) {
+        super(context, null);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        setMeasuredDimension(width, width);
+    }
+}
